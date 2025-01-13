4ac6807f9fd23d4818b7205ab544f66c2d8e7e4d
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-01-13 15:04:08 +0530

    initial import

---
+++ string/src/com/vaishnavi/practice/strings/BufferAndBuilderDemo.java
@@ -0,0 +1,16 @@
+package com.vaishnavi.practice.strings;
+
+public class BufferAndBuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder builderCity = new StringBuilder();
+        builderCity.append("latur");
+        System.out.println("builder city: " + builderCity);
+        System.out.println("builder city.capacity(): " + builderCity.capacity());
+
+        StringBuffer bufferCity = new StringBuffer();
+        bufferCity.append("pune");
+        System.out.println("buffer city: " + bufferCity);
+        System.out.println("buffer city.capacity(): " + bufferCity.capacity());
+    }
+}
+
