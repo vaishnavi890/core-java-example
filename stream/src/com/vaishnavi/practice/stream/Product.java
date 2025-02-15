160c62f676ec06f27359b76448342635cb722b88
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-02-15 16:02:05 +0530

    stream example

---
+++ stream/src/com/vaishnavi/practice/stream/Product.java
@@ -0,0 +1,17 @@
+package com.vaishnavi.practice.stream;
+
+// public class Product {
+   // int id;
+    //    String name;
+//    float price;
+//
+//    public Product(int id, String name, float price) {
+//        this.id = id;
+//        this.name = name;
+//        this.price = price;
+//    }
+//}
+    record Product(int id, String name, float price) {
+        // No need for an explicit constructor or other methods
+    }
+
