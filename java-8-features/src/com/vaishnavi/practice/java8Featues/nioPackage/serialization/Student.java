e52554b715e6f84b115d9bd067ed05817c4fe0f6
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-03-07 13:58:24 +0530

    initial commit

---
+++ java-8-features/src/com/vaishnavi/practice/java8Featues/nioPackage/serialization/Student.java
@@ -0,0 +1,39 @@
+package com.vaishnavi.practice.java8Featues.nioPackage.serialization;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private int rollNo;
+    private String name;
+    private Address address;
+
+    public Student(int rollNo, String name, Address address) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student: {" +
+                "rollNo=" + rollNo +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    static class Address implements Serializable {
+        String city;
+
+        public Address(String city) {
+            this.city = city;
+        }
+
+        @Override
+        public String toString() {
+            return "Address{" +
+                    "city='" + city + '\'' +
+                    '}';
+        }
+    }
+}
