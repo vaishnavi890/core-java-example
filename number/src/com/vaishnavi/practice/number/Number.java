c2fd8e03843a180f80243325c59b4a8f9faebb27
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-19 22:26:17 +0530

    initial import

---
+++ number/src/com/vaishnavi/practice/number/Number.java
@@ -0,0 +1,25 @@
+package com.vaishnavi.practice.number;
+
+import java.util.Scanner;
+
+public class Number {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Please enter positive number: ");
+        int no = sc.nextInt();
+
+        int[] digit = new int[String.valueOf(no).length()];
+        int index = -1;
+        while (no > 0) {
+            digit[++index] = no % 10;
+            no = no / 10;
+        }
+        index = digit.length;
+        System.out.println("Split digit:");
+        while (--index >= 0) {
+            System.out.println(digit[index]);
+        }
+    }
+}
+
