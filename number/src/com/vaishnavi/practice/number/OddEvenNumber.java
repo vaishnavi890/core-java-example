fac0e1c6fda2f38d2c1b4999a342e64010909b7b
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-20 13:22:29 +0530

    initial import

---
+++ number/src/com/vaishnavi/practice/number/OddEvenNumber.java
@@ -0,0 +1,13 @@
+package com.vaishnavi.practice.number;
+
+public class OddEvenNumber {
+    public static void main(String[] args) {
+        int no = 219;
+        if (no % 2 == 0) {
+            System.out.println("no is even");
+        } else {
+            System.out.println("no is odd");
+        }
+    }
+}
+
