9c1df392b2fa954cdb5e6dbdf5ea6e224113e131
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-24 18:14:16 +0530

    dateTime example

---
+++ dateTime/src/com/vaishnavi/practice/dateTime/StudentDateTimeExample.java
@@ -0,0 +1,9 @@
+package com.vaishnavi.practice.dateTime;
+
+public class StudentDateTimeExample {
+    public static void main(String[] args) {
+        Student s1 = new Student(1, "Vaishnavi");
+        s1.displayInfo();
+    }
+}
+
