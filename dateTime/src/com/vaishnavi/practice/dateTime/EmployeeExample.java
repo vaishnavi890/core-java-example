45e67ed22c02a7da0309f3685650de6954ab6f39
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-24 18:18:54 +0530

    dateTime example

---
+++ dateTime/src/com/vaishnavi/practice/dateTime/EmployeeExample.java
@@ -0,0 +1,9 @@
+package com.vaishnavi.practice.dateTime;
+
+public class EmployeeExample {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee(101, "Vaishnavi", 45000.00);
+        emp1.displayInfo();
+    }
+}
+
