7dcf14040ac2ddc9fbbb2fc209eeeafc8b6268cd
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-27 19:28:58 +0530

    initial import

---
+++ serializationDeserialization/src/com/vaishnavi/practice/serializationDeserialization/model/Employee.java
@@ -0,0 +1,37 @@
+package com.vaishnavi.practice.serializationDeserialization.model;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    // Getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public double getSalary() { return salary; }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
+
