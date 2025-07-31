d3a5da9f9db79b96c463f2cbe1d02ca06e658a15
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-31 10:36:05 +0530

    initial import

---
+++ garbageCollector/src/com/vaishnavi/practice/garbageCollector/gc/Person.java
@@ -0,0 +1,18 @@
+package com.vaishnavi.practice.garbageCollector.gc;
+
+public class Person {
+
+    private String name;
+
+    public Person(String alice) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
