977e2cfee78e5caf9dde55a6eaff24327b234e2d
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-01 15:02:01 +0530

    initial import

---
+++ java-8-features/src/com/vaishnavi/practice/java8Featues/defaultMethod/Car.java
@@ -0,0 +1,11 @@
+package com.vaishnavi.practice.java8Featues.defaultMethod;
+
+class Car implements Vehicle {
+    @Override
+    public void start() {
+        System.out.println("Car started");
+    }
+}
+
+
+
