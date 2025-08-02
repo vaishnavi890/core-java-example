980daf64d68a1e506af6defed59690f89e715c65
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-02 13:34:15 +0530

    initial import

---
+++ staticClass/src/com/vaishnavi/practice/staticClass/localInner/OuterForLocalInnerClass.java
@@ -0,0 +1,15 @@
+package com.vaishnavi.practice.staticClass.localInner;
+
+public class OuterForLocalInnerClass {
+    private final String message = "hello from outer class";
+
+    void outerMethod() {
+        class LocalInner {
+            void printMessage() {
+                System.out.println("msg " + message);
+            }
+        }
+        LocalInner localInner = new LocalInner();
+        localInner.printMessage();
+    }
+}
