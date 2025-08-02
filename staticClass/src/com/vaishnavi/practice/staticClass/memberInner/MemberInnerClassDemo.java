11ef76be7c6b086b07dced19432276e84aa7eedf
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-02 13:36:04 +0530

    initial import

---
+++ staticClass/src/com/vaishnavi/practice/staticClass/memberInner/MemberInnerClassDemo.java
@@ -0,0 +1,9 @@
+package com.vaishnavi.practice.staticClass.memberInner;
+
+public class MemberInnerClassDemo {
+    public static void main(String[] args) {
+        ParentOuter parentOuter = new ParentOuter();
+        ParentOuter.MemberInner memberInner = parentOuter.new MemberInner();
+        memberInner.printMessage();
+    }
+}
