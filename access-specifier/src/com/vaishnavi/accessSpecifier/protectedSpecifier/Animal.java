577439fa7f997570897f4887e718c4d64f0acb0e
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-12 12:16:21 +0530

    initial import

---
+++ access-specifier/src/com/vaishnavi/accessSpecifier/protectedSpecifier/Animal.java
@@ -0,0 +1,9 @@
+package com.vaishnavi.accessSpecifier.protectedSpecifier;
+
+public class Animal {
+    protected String name;
+
+    protected void displayName() {
+        System.out.println("Animal name: " + name);
+    }
+}
