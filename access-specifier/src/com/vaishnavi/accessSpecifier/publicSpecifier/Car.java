025a9b84f8a6375d9231419446750aef1080269f
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-12 12:22:33 +0530

    initial import

---
+++ access-specifier/src/com/vaishnavi/accessSpecifier/publicSpecifier/Car.java
@@ -0,0 +1,10 @@
+package com.vaishnavi.accessSpecifier.publicSpecifier;
+
+public class Car {
+    public String model;
+
+    public void displayModel() {
+        System.out.println("Car model: " + model);
+    }
+}
+
