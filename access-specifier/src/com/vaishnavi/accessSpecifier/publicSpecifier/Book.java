daa81783014f09216e59d2226753ad1d0792d24a
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-12 12:26:43 +0530

    initial import

---
+++ access-specifier/src/com/vaishnavi/accessSpecifier/publicSpecifier/Book.java
@@ -0,0 +1,12 @@
+package com.vaishnavi.accessSpecifier.publicSpecifier;
+
+public class Book {
+    public String title;
+    public String author;
+
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
+}
+
