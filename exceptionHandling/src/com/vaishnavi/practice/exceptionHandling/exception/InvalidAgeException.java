73e3ccb09fd15b88419bdfd32583903f1c4bf969
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-15 14:04:03 +0530

    initial import

---
+++ exceptionHandling/src/com/vaishnavi/practice/exceptionHandling/exception/InvalidAgeException.java
@@ -0,0 +1,7 @@
+package com.vaishnavi.practice.exceptionHandling.exception;
+
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
