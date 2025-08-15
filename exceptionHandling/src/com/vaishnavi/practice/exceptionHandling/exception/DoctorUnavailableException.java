b66716e742714ad9edc34d05dba61f3009cf7009
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-15 14:06:03 +0530

    initial import

---
+++ exceptionHandling/src/com/vaishnavi/practice/exceptionHandling/exception/DoctorUnavailableException.java
@@ -0,0 +1,7 @@
+package com.vaishnavi.practice.exceptionHandling.exception;
+
+public class DoctorUnavailableException extends Exception {
+    public DoctorUnavailableException(String message) {
+        super(message);
+    }
+}
