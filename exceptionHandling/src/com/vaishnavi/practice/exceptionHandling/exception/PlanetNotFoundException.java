ffe151bfc571e3bd11d8b7f3e1628b041f8c433d
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-15 13:52:00 +0530

    initial import

---
+++ exceptionHandling/src/com/vaishnavi/practice/exceptionHandling/exception/PlanetNotFoundException.java
@@ -0,0 +1,8 @@
+package com.vaishnavi.practice.exceptionHandling.exception;
+
+class PlanetNotFoundException extends Exception {
+    public PlanetNotFoundException(String message) {
+        super(message);
+    }
+}
+
