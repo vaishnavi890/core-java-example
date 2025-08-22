d5019e88b2029a0db8cf21979d5ac59093350f89
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-22 13:53:44 +0530

    initial import

---
+++ loop/src/com/vaishnavi/loop/pattern/doWhileLoop/DoWhileLoopDemo.java
@@ -0,0 +1,14 @@
+package com.vaishnavi.loop.pattern.doWhileLoop;
+
+public class DoWhileLoopDemo {
+    //pseudocode
+    //print no 1 to 5.using dp while loop.
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i <= 5);
+    }
+}
