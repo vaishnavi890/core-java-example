a816f0a6745602826da1bf64adec86b15613c9e0
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-22 13:57:24 +0530

    initial import

---
+++ loop/src/com/vaishnavi/loop/pattern/doWhileLoop/NaturalNo.java
@@ -0,0 +1,12 @@
+package com.vaishnavi.loop.pattern.doWhileLoop;
+
+public class NaturalNo {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+
+        } while (i <= 10);
+    }
+}
