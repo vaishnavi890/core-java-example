86c9075b1b8cb8b073e364dd26bd7697180529b9
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-23 14:06:25 +0530

    initial import

---
+++ loop/src/com/vaishnavi/loop/pattern/forLoop/ForLoopExample.java
@@ -0,0 +1,10 @@
+package com.vaishnavi.loop.pattern.forLoop;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // Using for loop to print numbers 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
