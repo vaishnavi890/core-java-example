d4ecc2f5a1f42eca8f969e93280666e4748b2089
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-23 13:44:22 +0530

    initial import

---
+++ loop/src/com/vaishnavi/loop/pattern/forLoop/ForLoop.java
@@ -0,0 +1,9 @@
+package com.vaishnavi.loop.pattern.forLoop;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("print value of i is = " + i);
+        }
+    }
+}
