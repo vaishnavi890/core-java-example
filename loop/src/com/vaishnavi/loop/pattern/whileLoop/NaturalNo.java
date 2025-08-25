74d00a83139d8c07dcd3465b5c18561a4e04b30f
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-25 13:22:32 +0530

    initial import

---
+++ loop/src/com/vaishnavi/loop/pattern/whileLoop/NaturalNo.java
@@ -0,0 +1,11 @@
+package com.vaishnavi.loop.pattern.whileLoop;
+
+public class NaturalNo {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("Natural number is " + i);
+            i++;
+        }
+    }
+}
