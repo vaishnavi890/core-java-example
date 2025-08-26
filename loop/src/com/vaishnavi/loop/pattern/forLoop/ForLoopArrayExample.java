5edfee093c4feba168c1ed6676450d16bf35ac5c
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-26 13:34:40 +0530

    initial import

---
+++ loop/src/com/vaishnavi/loop/pattern/forLoop/ForLoopArrayExample.java
@@ -0,0 +1,12 @@
+package com.vaishnavi.loop.pattern.forLoop;
+
+public class ForLoopArrayExample {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Mango", "Orange"};
+
+        // Iterate over array
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println("Fruit: " + fruits[i]);
+        }
+    }
+}
