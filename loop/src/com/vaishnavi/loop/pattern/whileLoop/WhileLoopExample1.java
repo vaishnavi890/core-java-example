b2fe3a2dee36e4da259888161feb2ea0bd9ace3e
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-26 13:24:55 +0530

    initial import

---
+++ loop/src/com/vaishnavi/loop/pattern/whileLoop/WhileLoopExample1.java
@@ -0,0 +1,13 @@
+package com.vaishnavi.loop.pattern.whileLoop;
+
+public class WhileLoopExample1 {
+    public static void main(String[] args) {
+        int number = 1; // initialization
+
+        // while loop to print numbers from 1 to 5
+        while (number <= 5) {
+            System.out.println("Number: " + number);
+            number++; // increment
+        }
+    }
+}
