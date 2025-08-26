c02cae78492b90a6926356d285813e73527ba022
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-26 13:32:43 +0530

    initial import

---
+++ loop/src/com/vaishnavi/loop/pattern/forLoop/ForLoopExample1.java
@@ -0,0 +1,10 @@
+package com.vaishnavi.loop.pattern.forLoop;
+
+public class ForLoopExample1 {
+    public static void main(String[] args) {
+        // Print numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
