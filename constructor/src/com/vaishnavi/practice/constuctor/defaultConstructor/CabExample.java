cc74f86b1124b419878725018b503aafeb52e241
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-27 10:26:30 +0530

    initial import

---
+++ constructor/src/com/vaishnavi/practice/constuctor/defaultConstructor/CabExample.java
@@ -0,0 +1,11 @@
+package com.vaishnavi.practice.constuctor.defaultConstructor;
+
+public class CabExample {
+    public static void main(String[] args) {
+        Cab cab1 = new Cab();
+        Cab cab2 = new Cab();
+
+        cab1.display();
+        cab2.display();
+    }
+}
