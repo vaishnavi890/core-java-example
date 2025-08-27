32de0b3ffa3b97ebff5c8f319a6c809a42a71d00
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-27 10:22:44 +0530

    initial import

---
+++ constructor/src/com/vaishnavi/practice/constuctor/defaultConstructor/BookExample.java
@@ -0,0 +1,11 @@
+package com.vaishnavi.practice.constuctor.defaultConstructor;
+
+public class BookExample {
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        Book b2 = new Book();
+
+        b1.display();
+        b2.display();
+    }
+}
