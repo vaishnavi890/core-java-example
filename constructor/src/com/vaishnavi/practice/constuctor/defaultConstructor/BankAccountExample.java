7b42515ee91a374d32b68a64612c11a19103c4bd
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-27 10:29:42 +0530

    initial import

---
+++ constructor/src/com/vaishnavi/practice/constuctor/defaultConstructor/BankAccountExample.java
@@ -0,0 +1,11 @@
+package com.vaishnavi.practice.constuctor.defaultConstructor;
+
+public class BankAccountExample {
+    public static void main(String[] args) {
+        BankAccount acc1 = new BankAccount();
+        BankAccount acc2 = new BankAccount();
+
+        acc1.display();
+        acc2.display();
+    }
+}
