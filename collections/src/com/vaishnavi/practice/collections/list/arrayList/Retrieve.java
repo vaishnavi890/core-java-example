b35ed38919a0b3511051fccf9489047b8a2c4974
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-30 11:35:27 +0530

    initial import

---
+++ collections/src/com/vaishnavi/practice/collections/list/arrayList/Retrieve.java
@@ -0,0 +1,20 @@
+package com.vaishnavi.practice.collections.list.arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Retrieve {
+    public static void main(String[] args) {
+
+        List<String> colors = new ArrayList<>();
+
+        colors.add("Pink");
+        colors.add("Red");
+        colors.add("Pink");
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Brown");
+        colors.add("Purple");
+        System.out.println("retrieve " + colors.get(3));
+    }
+}
