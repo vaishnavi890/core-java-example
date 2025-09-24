0853680afdd1520e2162384b7ef13e31cb73e7ce
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-09-24 13:43:34 +0530

    initial import

---
+++ array/src/com/vaishnavi/practice/array/ArrayRecap.java
@@ -0,0 +1,10 @@
+package com.vaishnavi.practice.array;
+
+public class ArrayRecap {
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int a : array)
+            System.out.println(a);
+    }
+}
