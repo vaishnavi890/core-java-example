87c0ec6d8e3e9be43cf1455359b2ca4d493d71e3
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-09-24 13:42:23 +0530

    initial import

---
+++ array/src/com/vaishnavi/practice/array/ArrayPractice.java
@@ -0,0 +1,14 @@
+package com.vaishnavi.practice.array;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        String[] name = new String[5];
+        name[0] = "shreya";
+        name[1] = "mahalle";
+        name[2] = "amt";
+        name[3] = "pune";
+        name[4] = "mumbai";
+
+        for (String string : name) System.out.println(string);
+    }
+}
