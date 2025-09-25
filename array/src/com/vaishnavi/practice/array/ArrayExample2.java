876a9bf77e18f14fd8b4df5aad116c0da7eec31a
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-09-25 15:09:41 +0530

    initial import

---
+++ array/src/com/vaishnavi/practice/array/ArrayExample2.java
@@ -0,0 +1,14 @@
+package com.vaishnavi.practice.array;
+
+public class ArrayExample2 {
+    public static void main(String[] args) {
+        // Declare and initialize array
+        String[] fruits = {"Apple", "Banana", "Mango", "Orange"};
+
+        // Enhanced for loop
+        System.out.println("Fruits list:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
