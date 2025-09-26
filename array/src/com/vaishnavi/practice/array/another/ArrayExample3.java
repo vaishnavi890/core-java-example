0ad4ff6fdd1910d9f6275a8a612554384ff51646
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-09-26 13:13:41 +0530

    initial import

---
+++ array/src/com/vaishnavi/practice/array/another/ArrayExample3.java
@@ -0,0 +1,20 @@
+package com.vaishnavi.practice.array.another;
+
+public class ArrayExample3 {
+    public static void main(String[] args) {
+        // 2D Array (Matrix)
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println("2D Array Elements:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
