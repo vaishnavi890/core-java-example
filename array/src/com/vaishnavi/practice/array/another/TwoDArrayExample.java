b99d55f0ea3c6089092511392e2ec87dd390a2b5
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-09-27 15:47:10 +0530

    initial import

---
+++ array/src/com/vaishnavi/practice/array/another/TwoDArrayExample.java
@@ -0,0 +1,19 @@
+package com.vaishnavi.practice.array.another;
+
+public class TwoDArrayExample {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println("2D Matrix Elements:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
