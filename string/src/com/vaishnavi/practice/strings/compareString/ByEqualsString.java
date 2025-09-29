299b4270e7f39282b006eddd5fa24f29089af86b
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-09-29 13:52:37 +0530

    string example

---
+++ string/src/com/vaishnavi/practice/strings/compareString/ByEqualsString.java
@@ -0,0 +1,14 @@
+package com.vaishnavi.practice.strings.compareString;
+
+public class ByEqualsString {
+    public static void main(String[] args) {
+        String s1 = "ram";
+        String s2 = "ram";
+        String s3 = "ram";
+        String s4 = "abc";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equals(s4));
+    }
+}
