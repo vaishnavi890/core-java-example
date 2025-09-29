574c8d1a514fb6150f40de4b8ebb5013eee228d9
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-09-29 13:54:13 +0530

    string example

---
+++ string/src/com/vaishnavi/practice/strings/compareString/ByOperator.java
@@ -0,0 +1,12 @@
+package com.vaishnavi.practice.strings.compareString;
+
+public class ByOperator {
+    public static void main(String[] args) {
+        String s1 = "ram";
+        String s2 = "ram";
+        String s3 = "ram";
+
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+    }
+}
