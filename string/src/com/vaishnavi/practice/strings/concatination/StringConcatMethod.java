61e81c03b495da5182757f213c4ae87ef0fb9c7a
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-10-01 13:47:44 +0530

    initial import

---
+++ string/src/com/vaishnavi/practice/strings/concatination/StringConcatMethod.java
@@ -0,0 +1,12 @@
+package com.vaishnavi.practice.strings.concatination;
+
+public class StringConcatMethod {
+    public static void main(String[] args) {
+        String s1 = "ram";
+        String s2 = "shyam";
+        String s3 = s1.concat(s2);
+        System.out.println(s3);
+        System.out.println(s1.concat(s2));
+
+    }
+}
