f1929aa48d04f14548f8c83052d5801a9131209d
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-10-01 13:46:16 +0530

    initial import

---
+++ string/src/com/vaishnavi/practice/strings/concatination/PrimitiveValue.java
@@ -0,0 +1,9 @@
+package com.vaishnavi.practice.strings.concatination;
+
+public class PrimitiveValue {
+    public static void main(String[] args) {
+
+        String s = 40 + 10 + "ram" + 40 + 40;
+        System.out.println(s);
+    }
+}
