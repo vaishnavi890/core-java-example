9c38fc674b7133efe67b9b2fbccd830440db1064
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-10-01 13:49:17 +0530

    initial import

---
+++ string/src/com/vaishnavi/practice/strings/concatination/StringConcatenationOperator.java
@@ -0,0 +1,8 @@
+package com.vaishnavi.practice.strings.concatination;
+
+public class StringConcatenationOperator {
+    public static void main(String[] args) {
+        String s = "shreya " + " shreya";
+        System.out.println("concatenation " + s);
+    }
+}
