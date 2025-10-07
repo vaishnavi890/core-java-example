0534302904d2caab65160fa9c1711c348127b07a
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-10-07 13:54:55 +0530

    initial import

---
+++ string/src/com/vaishnavi/practice/strings/stringLiteral/Literal.java
@@ -0,0 +1,8 @@
+package com.vaishnavi.practice.strings.stringLiteral;
+
+public class Literal {
+    public static void main(String[] args) {
+        String name = "Shreya";
+        System.out.println("name is " + name);
+    }
+}
