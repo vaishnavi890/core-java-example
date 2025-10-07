7692dcfab91562efeec262f46c24005d68b8a731
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-10-07 13:56:35 +0530

    initial import

---
+++ string/src/com/vaishnavi/practice/strings/stringLiteral/StringLiteral.java
@@ -0,0 +1,8 @@
+package com.vaishnavi.practice.strings.stringLiteral;
+
+public class StringLiteral {
+    public static void main(String[] args) {
+        String str = "cat";
+        System.out.println(str);
+    }
+}
