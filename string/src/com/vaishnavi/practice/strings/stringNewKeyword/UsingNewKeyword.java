c1d12b5b56e818b3728c1518f1831da4ff740cf0
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-10-07 14:00:23 +0530

    initial import

---
+++ string/src/com/vaishnavi/practice/strings/stringNewKeyword/UsingNewKeyword.java
@@ -0,0 +1,11 @@
+package com.vaishnavi.practice.strings.stringNewKeyword;
+
+public class UsingNewKeyword {
+    public static void main(String[] args) {
+        String name = "Welcome";
+        name = name.concat(" pune "); //immutable string
+
+        System.out.println(name);
+
+    }
+}
