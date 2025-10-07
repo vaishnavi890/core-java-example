044193b1c525e450aa95b567a533a679921a8031
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-10-07 13:58:44 +0530

    initial import

---
+++ string/src/com/vaishnavi/practice/strings/stringNewKeyword/StringObject.java
@@ -0,0 +1,8 @@
+package com.vaishnavi.practice.strings.stringNewKeyword;
+
+public class StringObject {
+    public static void main(String[] args) {
+        String str = "shreya";
+        System.out.println(str);
+    }
+}
