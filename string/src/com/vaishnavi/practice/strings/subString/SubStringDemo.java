38a747bfc4e345d2d1fd6d7c26b6b4fc663f37c3
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-10-07 14:02:31 +0530

    string example

---
+++ string/src/com/vaishnavi/practice/strings/subString/SubStringDemo.java
@@ -0,0 +1,11 @@
+package com.vaishnavi.practice.strings.subString;
+
+public class SubStringDemo {
+    public static void main(String[] args) {
+        String s1 = "Sachin Tendulkar";
+        System.out.println(s1.substring(5));
+        System.out.println(s1.substring(1));
+
+        //System.out.println(s1.substring(0,6));
+    }
+}
